6cc7f4fd9f70cd30ea540b92cbe610fd91dd18e3
Tim <15017472+doodlum@users.noreply.github.com>
2025-03-02 20:54:34 +0000

    fix: remove ui composition

--- src/XSEPlugin.cpp
+++ src/XSEPlugin.cpp
@@ -88,7 +88,6 @@ void MessageHandler(SKSE::MessagingInterface::Message* message)
 				}
 				Hooks::Install();
 				FrameAnnotations::OnPostPostLoad();
-				DX12SwapChain::InstallHooks();
 
 				auto shaderCache = globals::shaderCache;
 
