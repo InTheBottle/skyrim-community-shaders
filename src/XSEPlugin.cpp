d2f61278b738f1b44a1c6bf2a3880cf82294dc3d
Tim <15017472+doodlum@users.noreply.github.com>
2025-03-03 17:30:20 +0000

    chore: remove option to disable fg/upscaling

--- src/XSEPlugin.cpp
+++ src/XSEPlugin.cpp
@@ -83,9 +83,7 @@ void MessageHandler(SKSE::MessagingInterface::Message* message)
 				state->PostPostLoad();  // state should load first so basic information is populated
 				Deferred::Hooks::Install();
 				globals::truePBR->PostPostLoad();
-				if (!state->IsFeatureDisabled("Upscaling")) {
-					Upscaling::InstallHooks();
-				}
+				Upscaling::InstallHooks();	
 				Hooks::Install();
 				FrameAnnotations::OnPostPostLoad();
 
