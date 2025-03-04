782acf98dce9878bdfeaa029eccd3c7b5f4307b7
Tim <15017472+doodlum@users.noreply.github.com>
2025-03-04 15:14:15 +0000

    feat: set up skyrim to receive upscalers

--- src/XSEPlugin.cpp
+++ src/XSEPlugin.cpp
@@ -86,6 +86,7 @@ void MessageHandler(SKSE::MessagingInterface::Message* message)
 				Upscaling::InstallHooks();
 				Hooks::Install();
 				FrameAnnotations::OnPostPostLoad();
+				DX12SwapChain::InstallHooks();
 
 				auto shaderCache = globals::shaderCache;
 
