3c9bba3a67a850ecf14f9609aea2556ac7e0621e
Tim <15017472+doodlum@users.noreply.github.com>
2025-08-04 01:47:42 +0100

    chore: plugin init things

--- src/XSEPlugin.cpp
+++ src/XSEPlugin.cpp
@@ -78,11 +78,8 @@ void MessageHandler(SKSE::MessagingInterface::Message* message)
 	case SKSE::MessagingInterface::kPostPostLoad:
 		{
 			if (errors.empty()) {
-				auto state = globals::state;
-				state->PostPostLoad();  // state should load first so basic information is populated
 				Deferred::Hooks::Install();
 				globals::truePBR->PostPostLoad();
-				Upscaling::InstallHooks();
 				Hooks::Install();
 				EngineFix::InstallOnPostPostLoadFixes();
 				FrameAnnotations::OnPostPostLoad();
@@ -95,6 +92,8 @@ void MessageHandler(SKSE::MessagingInterface::Message* message)
 						feature->PostPostLoad();
 					}
 				}
+
+				Upscaling::InstallHooks();
 
 				// Now validate disk cache after features have had a chance to modify their state
 				shaderCache->ValidateDiskCache();
@@ -169,7 +168,8 @@ bool Load()
 
 	const std::array dlls = {
 		L"Data/SKSE/Plugins/ShaderTools.dll",
-		L"Data/SKSE/Plugins/SSEShaderTools.dll"
+		L"Data/SKSE/Plugins/SSEShaderTools.dll",
+		L"Data/SKSE/Plugins/SkyrimUpscaler.dll"
 	};
 
 	for (const auto dll : dlls) {
